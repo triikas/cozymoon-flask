cfd40ef64dfbae36cfb3324725f078b6b0cf8764
TriPC <triikas@triikas.ru>
2024-07-08 18:06:06 +0300

    1

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ application = Flask(__name__)
 def hello():
    return render_template("base.html")
 
+
+@application.route("/minecraft")
+def minecraft():
+   return render_template("minecraft.html")
+
 if __name__ == "__main__":
    application.run(host='0.0.0.0')
---
+++ static/func.js
@@ -0,0 +1,7 @@
+function menu_in() {
+    document.querySelector(".menu").classList.remove("d-none");
+}
+
+function menu_out() {
+    document.querySelector(".menu").classList.add("d-none");
+}
